73548af38514e56f4e2d4c0fa45c75a2ed8bd063
晃一 <kouichi@kouichi-no-MacBook-Air.local>
2016-01-26 22:20:42 +0900

    テーブル表示処理を追加

---
+++ PasswordMemo/PasswordInputView.swift
@@ -0,0 +1,21 @@
+//
+//  PasswordInputView.swift
+//  PasswordMemo
+//
+//  Created by 晃一 on 2016/01/26.
+//  Copyright © 2016年 HIGHCOM. All rights reserved.
+//
+
+import UIKit
+
+class PasswordInputView: UIViewController {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- PasswordMemo/PasswordListView.swift
+++ PasswordMemo/PasswordListView.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class PasswordListView: UIViewController {
+class PasswordListView: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var passwordListView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // テーブルの行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    // テーブルの表示内容
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = passwordListView.dequeueReusableCellWithIdentifier("PasswordCell")! as UITableViewCell
+        // タイトルタグを取得
+        let title = cell.viewWithTag(1) as! UILabel
+        title.text = "Title"
+        
+        // 日付タグを取得
+        let date = cell.viewWithTag(2) as! UILabel
+        date.text = "2015/12/31"
+        
+        return cell
     }
     
     override func didReceiveMemoryWarning() {
