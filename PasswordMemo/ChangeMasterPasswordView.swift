60f038fb0a5ef0b7dec5d2122e52b825487c7c0f
晃一 <kouichi@kouichi-no-MacBook-Air.local>
2016-03-19 10:36:48 +0900

    マスターパスワード変更画面処理の追加

--- PasswordMemo/ChangeMasterPasswordView.swift
+++ PasswordMemo/ChangeMasterPasswordView.swift
@@ -10,12 +10,35 @@ import UIKit
 
 class ChangeMasterPasswordView: UIViewController {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
+    @IBOutlet weak var inputPassword1: UITextField!
+    @IBOutlet weak var inputPassword2: UITextField!
 
+    @IBOutlet weak var checkResultLabel: UILabel!
+    
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        inputPassword1.secureTextEntry = true
+        inputPassword2.secureTextEntry = true
+    }
+    
+    @IBAction func checkPassword(sender: AnyObject) {
+        var masterPassword: String?
+        masterPassword = userDefaults.objectForKey("masterPw") as? String
+        if inputPassword1.text != inputPassword2.text {
+            // 入力が違っていたらエラー
+            checkResultLabel.text = "Password is different!"
+        } else if masterPassword == inputPassword1.text {
+            // マスターパスワードと同じだったらエラー
+            checkResultLabel.text = "It is the same as the master password"
+        } else {
+            // マスターパスワードが作成されていない場合は新規作成
+            userDefaults.setObject(inputPassword1.text, forKey: "masterPw")
+            userDefaults.synchronize()
+            // 画面を終了する
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
 }
